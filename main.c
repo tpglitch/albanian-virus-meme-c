25d9624a52c3baff70bebfc5a3ffffe8a53991e5
Tyler Kinney <tylerkinney50@gmail.com>
2024-08-22 15:34:28 -0500

    Initial commit

---
+++ main.c
@@ -0,0 +1,7 @@
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow)
+{
+    MessageBox(NULL, "Hi, I am Albanian virus but because of poor technology in my country, unfortunately I am unable to harm your computer. Please be so kind to delete one of your important files yourself and then forward me to other users. Many thanks for your cooperation. Best regards,Albanian Virus.", "Microsoft(R) WINDOWS(R) Error Box - VIRUS", MB_OK);
+    return 0;
+}
